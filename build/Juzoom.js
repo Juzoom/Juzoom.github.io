76983de8333875b43d56ac7c0ee37b782748337b
topPC_zk <1120429511@qq.com>
2016-12-22 22:33:21 +0800

    yuying

--- build/Juzoom.js
+++ build/Juzoom.js
@@ -56,6 +56,21 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	__webpack_require__(1);
 	impress().init();
+	// https://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&text=123
+	var frame = document.createElement('iframe');
+	frame.height=0;
+	frame.width=0;
+	frame.scrolling='no';
+	frame.frameborder='0';
+	frame.src='https://tts.baidu.com/text2audio?lan=en&ie=UTF-8&text=we are awesome';
+	document.body.appendChild(frame);
+
+	var rootElement = document.getElementById( "impress" );
+	rootElement.addEventListener( "impress:stepenter", function(event) {
+		var currentStep = event.target;
+		var curDom = document.getElementById(currentStep.id);
+		console.log(curDom)
+	});
 
 /***/ },
 /* 1 */
